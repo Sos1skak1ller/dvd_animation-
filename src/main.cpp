d44bab8f9269c66b8c9ffba26a76cd3ec125cc9e
Михаил Быковский <sosiska_killer@Noutbuk-Mihail.local>
2023-11-02 09:59:49 +0300

    1st test

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,40 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Отталкивание от стенок");
+
+    sf::CircleShape circle(30.0f);
+    circle.setFillColor(sf::Color::Blue);
+    circle.setPosition(100.0f, 100.0f);
+
+    float velocityX = 5.0f;
+    float velocityY = 5.0f;
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
+        }
+
+    
+        circle.move(velocityX, velocityY);
+
+        if (circle.getPosition().x < 0 || circle.getPosition().x + 2 * circle.getRadius() > window.getSize().x)
+        {
+            velocityX = -velocityX; 
+        }
+        if (circle.getPosition().y < 0 || circle.getPosition().y + 2 * circle.getRadius() > window.getSize().y)
+        {
+            velocityY = -velocityY; 
         }
 
         window.clear();
+        window.draw(circle);
         window.display();
     }
+
+    return 0;
 }
